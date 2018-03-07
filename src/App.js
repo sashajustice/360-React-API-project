eeb7daeac59fd12b49f9c1aec96d1b4bd737a80a
sashajustice <sabrinnasreddin@gmail.com>
2018-03-07 14:12:22 -0800

    begin to add map components and container etc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+
+
 import ReactDOM from 'react-dom';//react library we use to interact w actual DOM
+import MapContainer from './components/mapContainer'
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_details';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import './App.css';
-
-
+import { GoogleApiWrapper } from 'google-maps-react'
 const API_KEY = 'AIzaSyDSeJoVI2CRT0Eub--ulZcIGISbgGUmXCI';
-
 
 
 
@@ -35,18 +39,28 @@ videoSearch(term) {
 }
 
 render() {
-  const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
-  return (
-    <div>
-      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
-      <VideoDetail video={this.state.selectedVideo} />
-      <VideoList
-      onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
-      videos={this.state.videos} />
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+    return (
+      <div>
+       <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+       <VideoDetail video={this.state.selectedVideo} />
+       <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+        <h1> Google Maps API + React </h1>  'google'
+        <MapContainer google={this.props.google} />
 
-    </div>
-  )};
-  }
+        </div>
+      );
+    }
+}
 
 
+
+// PART 2: Take this components generated HTML and put it on the page ( in the DOM)
+//App << this is a class of the component.
  ReactDOM.render(<App />, document.querySelector('.container'));
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDSeJoVI2CRT0Eub--ulZcIGISbgGUmXCI',
+})(App)
---
+++ src/components/mapContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+const API_KEY = 'd47538034411e4526726eb4345543'
+
+class MapContainer extends Component {
+
+  componentDidUpdate() {
+    this.loadMap();//call loadMap function to load the google map
+  }
+
+
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      const {google} = this.props;
+      const maps = google.maps;
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);//finds the 'map' div in teh React dom, names it
+      const mapConfig = Object.assign({}, {
+        center: {lat: 37.8044, lng: 122.2711},
+        zoom: 11,
+        mapTypeId: 'roadmap'
+      })
+      this.map = new maps.Map(node, mapConfig);
+    }
+  }
+
+  render() {
+    const style = {
+      width: '90vw',
+      height: '75vh'
+    }
+
+    return (
+      <div ref="map" style={style}>
+         loading map...
+      </div>
+    )
+  }
+}
+
+
+export default MapContainer;
